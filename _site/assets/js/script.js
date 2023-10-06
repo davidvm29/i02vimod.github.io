75399da92c75ac0106b606482f4159f788f9165f
David Villen Moreno <i02vimod@uco.es>
2023-10-06 13:51:42 +0200

    prueba

--- _site/assets/js/script.js
+++ _site/assets/js/script.js
@@ -12,3 +12,47 @@ function changeLanguage(language) {
         document.getElementById("results").innerHTML = "Resultados de búsqueda para: ";
     }
 }
+
+// Función para mostrar u ocultar el menú desplegable
+function toggleDropdown() {
+  var dropdown = document.getElementById("mainDropdown");
+  if (dropdown.style.display === "none" || dropdown.style.display === "") {
+    dropdown.style.display = "block";
+  } else {
+    dropdown.style.display = "none";
+  }
+}
+
+  
+  // Función para mostrar u ocultar las subopciones
+  function toggleSubDropdown(event) {
+    var subDropdown = event.nextElementSibling;
+    subDropdown.classList.toggle("show");
+  }
+  
+  // Cerrar el menú desplegable si el usuario hace clic fuera de él
+  window.onclick = function (event) {
+    if (!event.target.matches('.dropdown-icon')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  $(document).ready(function(){
+    $('.slider').slick({
+      autoplay: true, // Hacer que el slider se reproduzca automáticamente
+      autoplaySpeed: 3000, // Cambiar de imagen cada 3 segundos (ajusta según tus preferencias)
+      dots: true, // Mostrar los puntos de navegación
+      arrows: true // Mostrar las flechas de navegación
+      // Puedes ajustar más opciones según tus necesidades
+    });
+  });
+  
+  
+  
+  
